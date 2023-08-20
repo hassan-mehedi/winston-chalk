0780d902c99503b5a79cf7af7877f7a8c8de9866
hassan-mehedi <howlader.mehedihassan@gmail.com>
2023-08-20 13:44:00 +0600

    fix logging issues

--- custom-logger.js
+++ custom-logger.js
@@ -3,31 +3,55 @@ const chalk = require("chalk");
 
 // Define custom colors using Chalk
 const customColors = {
-    info: "blue",
+    info: "green",
     warn: "yellow",
     error: "red",
+    debug: "magenta",
 };
 
 // Create a Chalk colorizer using the custom colors
 const colorizer = winston.format.colorize({ colors: customColors });
 
-// Create a custom Chalk logger
-const chalkLogger = {
-    info: chalk.blue,
-    warn: chalk.yellow,
-    error: chalk.red,
-};
-
 // Create a custom format that applies Chalk colors
 const customFormat = winston.format.printf(({ timestamp, level, message }) => {
+    // Make color type string to match with level
+    const infoType = chalk.green("info") === level;
+    const warnType = chalk.yellow("warn") === level;
+    const errorType = chalk.red("error") === level;
+
     // Apply Chalk color based on log level
-    const coloredLevel = chalkLogger[level](`[${level}]`);
-    return `${timestamp} ${coloredLevel} ${message}`;
+    const infoColorTime = chalk.greenBright(`${timestamp}`);
+    const warnColorTime = chalk.yellowBright(`${timestamp}`);
+    const errorColorTime = chalk.redBright(`${timestamp}`);
+    const debugColorTime = chalk.magentaBright(`${timestamp}`);
+
+    // Apply Chalk color on log
+    const infoColorValue = chalk.greenBright("INFO");
+    const warnColorValue = chalk.yellowBright("WARN");
+    const errorColorValue = chalk.redBright("ERROR");
+    const debugColorValue = chalk.magentaBright("DEBUG");
+
+    // Apply Chalk color based on log level
+    const infoColorMessage = chalk.greenBright(`${message}`);
+    const warnColorMessage = chalk.yellowBright(`${message}`);
+    const errorColorMessage = chalk.redBright(`${message}`);
+    const debugColorMessage = chalk.magentaBright(`${message}`);
+
+    // Apply timestamp color
+    const coloredTimestamp = infoType ? infoColorTime : warnType ? warnColorTime : errorType ? errorColorTime : debugColorTime;
+
+    // Apply log color
+    const coloredLevel = infoType ? infoColorValue : warnType ? warnColorValue : errorType ? errorColorValue : debugColorValue;
+
+    // Add message color
+    const coloredMessage = infoType ? infoColorMessage : warnType ? warnColorMessage : errorType ? errorColorMessage : debugColorMessage;
+
+    return `» [${coloredTimestamp} − ${coloredLevel}] − ${coloredMessage}`;
 });
 
 // Create the Winston logger
 const logger = winston.createLogger({
-    level: "info",
+    level: "debug",
     format: winston.format.combine(
         winston.format.timestamp({ format: () => new Date().toLocaleTimeString() }),
         colorizer, // Apply Chalk colors
---
+++ test.js
@@ -0,0 +1,6 @@
+const logger = require("./index");
+
+logger.info("🚀 Server started on port 3000");
+logger.warn("⚠️  Warning: Disk space is running low");
+logger.error("❌ Error: Database connection failed");
+logger.debug("🔍 Debugging information: ...");
